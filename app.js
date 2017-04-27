1d84ad0afd47df8d7364718ed92b7bda53f4ac82
Kiehls <nonestrike@gmail.com>
2017-04-27 22:01:32 +0900

    Add TCP Socket Module

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var chat = require('./routes/chat');
 
 var app = express();
 
@@ -19,38 +18,43 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-//----------------------------------------User Inserted Code---------------------------
+
+//-----------------------------Connection Between Unity Game Engine--------------------
 var Socket = require('socket.io');
 var socketEvents = require('./socket.js');
 const server = app.listen(6110, function () {
-    console.log("Express App on port 6110");
+    console.log("Unity Socket App on port 6110");
 });
 const io = new Socket(server);
 socketEvents(io);
-//-------------------------------------------------------------------------------------
+//-----------------------------Inter Connection Between Graph Module-------------------
+var net = require('net');
+var InterSocketEvents = require('./TCPSocket');
+InterSocketEvents(net);
+//=====================================================================================
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- socket.js
+++ socket.js
@@ -3,8 +3,8 @@
  */
 
 //io = server , socket = client(Android)
-module.exports = function(io) {
-    io.on('connection', function(socket) {
+module.exports = function (io) {
+    io.on('connection', function (socket) {
         console.log('Socket initiated!');
 
         //Android starts Game(First Connection).
@@ -12,33 +12,36 @@ module.exports = function(io) {
         //socket.emit('App start', 'Initialize Korean Chess Game');
 
         //Android send request & server send response
-        socket.on('App request', function(pos) {
-            console.log('Client Position: ', pos);
-            //This part should be changed to graph
+        socket.on('App request', function (pos) {
+            //console.log('Client Position: ', pos);
+            //console.log('Client Data Type: ', typeof (pos)) //-->String
+
+            // //This part should be generate next state node
             var next = JSON.parse(pos);
             console.log("Received Before Position: " + next.B_Pos + "\n" +
                 "Received After Position: " + next.A_Pos);
+            console.log("Board Data: \n" + next.Board);
 
             var B_Xpos = Math.floor(parseInt(next.B_Pos) / 10);
             var B_Ypos = parseInt(next.B_Pos) % 10;
             var A_Xpos = Math.floor(parseInt(next.A_Pos) / 10);
             var A_Ypos = parseInt(next.A_Pos) % 10;
+
             console.log("B_xPos: " + B_Xpos + ", B_yPos: " + B_Ypos);
             console.log("A_xPos: " + A_Xpos + ", A_yPos: " + A_Ypos);
 
             var nextPos = {
-                "B_Pos" : parseInt(next.B_Pos) + 1,
-                "A_Pos" : parseInt(next.A_Pos) + 1,
-                "Unit" : "O"
+                "B_Pos": parseInt(next.B_Pos) + 1,
+                "A_Pos": parseInt(next.A_Pos) + 1,
             };
-            //Make JSON data to response to client
             var nextJson = JSON.stringify(nextPos);
+
             io.emit('App response', nextJson);
             console.log("JSON Data Transmit: " + nextJson);
         });
         socket.on('App disconnect', function () {
-           socket.disconnect();
-           console.log('Disconnect from user');
+            socket.disconnect();
+            console.log('Disconnect from user');
         });
     });
 };
